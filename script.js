fd7e42f90e59e8482622fe7a3df15204335f82f2
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-08 20:09:43 -0500

    leyendo html desde js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!')
+// leemos elementos html con querySelector
+const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const pClass = document.querySelector('.parrafito')
+const pID = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+// notemos que querySelector recibe un string con formato CSS
+// otra opción para leer elementos HTML serían usando:
+// (1) const pClass = document.getElementsByClassName('parrafito')
+// (2) const pID = document.getElementById('pid')
+
+// si necesitamos mostrar varios elementos usando console.log()
+// podemos colocarlos en un objeto {} para mostrar todos a la vez
+console.log({h1, p, pClass, pID, input})
+
+// podemos acceder desde js a los atributos html de nuestros elementos
+console.log(input.value)
