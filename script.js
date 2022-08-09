d5a4bf06699e97ff76452961326492f19e5ea28d
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-08 21:26:15 -0500

    escribiendo html desde js

--- script.js
+++ script.js
@@ -5,14 +5,41 @@ const pClass = document.querySelector('.parrafito')
 const pID = document.querySelector('#pid')
 const input = document.querySelector('input')
 
-// notemos que querySelector recibe un string con formato CSS
-// otra opción para leer elementos HTML serían usando:
-// (1) const pClass = document.getElementsByClassName('parrafito')
-// (2) const pID = document.getElementById('pid')
+// innerHTML permite modificar directamente archivos html
+// esto es peligroso y se recomienda, en vez, usar innerText
+h1.innerHTML = 'Patito <br>Feo' // inserta un salto de linea
+h1.innerText = 'Patito <br>Feo' // inserta solo texto
 
-// si necesitamos mostrar varios elementos usando console.log()
-// podemos colocarlos en un objeto {} para mostrar todos a la vez
-console.log({h1, p, pClass, pID, input})
+// la manera mas comun de modificar atributos html es con:
+// (1) getAttribute, donde obtenemos su valor desde HTML
+// (2) setAttribute, con el insertamos un atributo y su valor
+console.log(h1.getAttribute('class'))
 
-// podemos acceder desde js a los atributos html de nuestros elementos
+h1.setAttribute('class', 'rojo')
+console.log(h1.getAttribute('class'))
+
+// hablando especificamente de clases podemos usar classList
+// para agregar o remover valores:
+h1.classList.add('azul')
+console.log(h1.getAttribute('class'))
+
+h1.classList.remove('rojo')
+console.log(h1.getAttribute('class'))
+
+// existen otras funciones de classList como toggle y contains
+
+// si el atributo ya existe podemos modificarlo así:
+input.value = '456'
 console.log(input.value)
+
+// para crear elementos html desde 0 usamos:
+const img = document.createElement('img')
+img.setAttribute('src', 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+
+// para visualizar el elemento anterior lo insertamos en un
+// elemento previamente creado en nuestro html
+pID.append(img)
+
+// reto: remover el texto de pid y mostrar la imagen
+pID.innerText = ''
+pID.appendChild(img)
